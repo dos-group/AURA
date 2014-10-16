4a1069cf776851291731297091c6cabfdb455ce5
TobiasHerb <tobias.herb@tu-berlin.de>
2014-10-16 19:43:25 +0200

    wip

--- aura-wm/src/main/java/de/tuberlin/aura/workloadmanager/TopologyScheduler.java
+++ aura-wm/src/main/java/de/tuberlin/aura/workloadmanager/TopologyScheduler.java
@@ -1,7 +1,17 @@
 package de.tuberlin.aura.workloadmanager;
 
 import de.tuberlin.aura.core.dataflow.api.DataflowNodeProperties;
+import de.tuberlin.aura.core.dataflow.operators.impl.HDFSSourcePhysicalOperator;
+import de.tuberlin.aura.core.filesystem.FileInputSplit;
+import de.tuberlin.aura.core.filesystem.InputSplit;
+import de.tuberlin.aura.core.filesystem.InputSplitAssigner;
+import de.tuberlin.aura.core.filesystem.LocatableInputSplitAssigner;
+import de.tuberlin.aura.core.filesystem.in.CSVInputFormat;
+import de.tuberlin.aura.core.filesystem.in.InputFormat;
 import de.tuberlin.aura.workloadmanager.spi.IInfrastructureManager;
+import de.tuberlin.aura.workloadmanager.spi.IWorkloadManager;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +23,12 @@ import de.tuberlin.aura.core.topology.Topology.ExecutionNode;
 import de.tuberlin.aura.core.topology.Topology.LogicalNode;
 import de.tuberlin.aura.core.topology.Topology.TopologyBreadthFirstTraverser;
 import de.tuberlin.aura.core.topology.TopologyStates.TopologyTransition;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 public class TopologyScheduler extends AssemblyPhase<AuraTopology, AuraTopology> {
@@ -78,5 +94,68 @@ public class TopologyScheduler extends AssemblyPhase<AuraTopology, AuraTopology>
                 }
             }
         });
+    }
+
+    public static class InputSplitManager implements Serializable {
+
+        // ---------------------------------------------------
+        // Fields.
+        // ---------------------------------------------------
+
+        private final IWorkloadManager workloadManager;
+
+        private Map<UUID, InputSplitAssigner> inputSplitMap;
+
+        // ---------------------------------------------------
+        // Fields.
+        // ---------------------------------------------------
+
+        public InputSplitManager(final IWorkloadManager workloadManager) {
+            // sanity check.
+            if (workloadManager == null)
+                throw new IllegalArgumentException("workloadManager == null");
+
+            this.workloadManager = workloadManager;
+
+            this.inputSplitMap = new ConcurrentHashMap<>();
+        }
+
+        // ---------------------------------------------------
+        // Public Methods.
+        // ---------------------------------------------------
+
+        public void registerHDFSSource(final LogicalNode node) {
+            if (node == null)
+                throw new IllegalArgumentException("node == null");
+            if (node.propertiesList.get(0) == null)
+                throw new IllegalStateException("properties == null");
+
+            final Path path = new Path((String)node.propertiesList.get(0).config.get(HDFSSourcePhysicalOperator.HDFS_SOURCE_FILE_PATH));
+            final Class<?>[] fieldTypes = (Class<?>[]) node.propertiesList.get(0).config.get(HDFSSourcePhysicalOperator.HDFS_SOURCE_INPUT_FIELD_TYPES);
+
+            @SuppressWarnings("unchecked")
+            final InputFormat inputFormat = new CSVInputFormat(path, fieldTypes);
+
+            final Configuration conf = new Configuration();
+            conf.set("fs.defaultFS", workloadManager.getConfig().getString("wm.io.hdfs.hdfs_url"));
+            inputFormat.configure(conf);
+
+            final InputSplit[] inputSplits;
+            try {
+                inputSplits = inputFormat.createInputSplits(node.propertiesList.get(0).globalDOP);
+            } catch (IOException e) {
+                throw new IllegalStateException(e);
+            }
+
+            final InputSplitAssigner inputSplitAssigner = new LocatableInputSplitAssigner((FileInputSplit[])inputSplits);
+            inputSplitMap.put(node.uid, inputSplitAssigner);
+        }
+
+        public synchronized InputSplit getInputSplitFromHDFSSource(final ExecutionNode exNode) {
+            if (exNode == null)
+                throw new IllegalArgumentException("exNode == null");
+            final InputSplitAssigner inputSplitAssigner = inputSplitMap.get(exNode.logicalNode.uid);
+            return inputSplitAssigner.getNextInputSplit(exNode);
+        }
     }
 }
