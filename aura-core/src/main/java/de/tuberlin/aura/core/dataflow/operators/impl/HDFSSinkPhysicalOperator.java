dee20cfbdfc73ad5ca92f0260b01aab239ec771d
Lauritz Thamsen <lauritz.thamsen@gmail.com>
2014-11-04 11:09:59 +0100

    Fixed HDFSSink

--- aura-core/src/main/java/de/tuberlin/aura/core/dataflow/operators/impl/HDFSSinkPhysicalOperator.java
+++ aura-core/src/main/java/de/tuberlin/aura/core/dataflow/operators/impl/HDFSSinkPhysicalOperator.java
@@ -42,7 +42,7 @@ public class HDFSSinkPhysicalOperator <I> extends AbstractUnaryPhysicalOperator<
     public void open() throws Throwable {
         super.open();
 
-        final Path path = new Path((String)getContext().getProperties().config.get(HDFS_SINK_FILE_PATH));
+        final Path path = new Path((String)getContext().getProperties(getOperatorNum()).config.get(HDFS_SINK_FILE_PATH));
 
         this.outputFormat = new CSVOutputFormat<AbstractTuple>(path);
 
@@ -52,7 +52,7 @@ public class HDFSSinkPhysicalOperator <I> extends AbstractUnaryPhysicalOperator<
 
         this.outputFormat.configure(conf);
 
-        this.outputFormat.open(getContext().getNodeDescriptor().taskIndex, getContext().getProperties().globalDOP);
+        this.outputFormat.open(getContext().getNodeDescriptor().taskIndex, getContext().getProperties(getOperatorNum()).globalDOP);
 
         this.inputOp.open();
     }
